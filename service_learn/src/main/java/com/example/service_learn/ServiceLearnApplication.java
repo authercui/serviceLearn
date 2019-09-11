b970af18d730d1f5c5d5dd7c487685cd8b40ecd6
cuidengchun <2573977943@qq.com>
2019-09-11 09:49:17 +0800

    初始化后台

---
+++ service_learn/src/main/java/com/example/service_learn/ServiceLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.service_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceLearnApplication.class, args);
+    }
+
+}
